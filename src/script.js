c6119f4bbb98b616347fae32964743f1f11ead67
soraia <soraiavercosa3@icloud.com>
2025-03-27 16:53:58 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,21 @@
+function usuario1() {
+    const nome = document.getElementById('fname').value
+    alert(`você escolheu o plano de 100 R$, ${nome}`)
+}
+
+function usuario2() {
+    const nome = document.getElementById('fname').value
+    alert(`você escolheu o plano 3x de 95 R$, ${nome}`)
+}
+
+function usuario3() {
+    const nome = document.getElementById('fname').value
+    alert(`você escolheu o plano 6x de 90 R$, ${nome}`)
+}
+
+function usuario4() {
+    const nome = document.getElementById('fname').value
+    alert(`você escolheu diaria de 30 R$, ${nome}`)
+}
+
+
